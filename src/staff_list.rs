37860e991812979def6bc7959328dac19a60aaa0
ender <ender_code@outlook.com>
2023-07-05 17:21:16 -0600

    new add_dept function

--- src/staff_list.rs
+++ src/staff_list.rs
@@ -1,26 +1,30 @@
 use std::collections::{HashMap, hash_map::Entry};
 
 pub struct Business {
-    dept_list: Vec<String>,
     staff_list: HashMap<String, Vec<String>>
 }
 
 impl Business {
     pub fn new() -> Business {
         Business {
-            dept_list: Vec::new(),
             staff_list: HashMap::new()
         }
     }
 
-    pub fn add_dept(&mut self, dept: &str) {
-        self.staff_list.entry(dept.to_string())
-        .or_insert(Vec::new());
+    pub fn add_dept(&mut self, dept: &str) -> Result<(), String> {
+        match self.staff_list.entry(dept.to_string()) {
+            Entry::Vacant(list) => {list.insert(Vec::new()); Ok(())},
+            Entry::Occupied(_) => Err(String::from("Department already exists"))
+        }
+        
     }
 
     pub fn add_employee(&mut self, dept: &str, name: &str) {
         match self.staff_list.entry(dept.to_string()) {
-            Entry::Vacant(list) => list.insert(Vec::new()).push(name.to_string()),
+            // if dept does not exist, add dept as new key and push name to dept's vec.
+            // Entry::Vacant(list) => list.insert(Vec::new()).push(name.to_string()),
+            Entry::Vacant(list) => self.add_dept(dept).unwrap(),
+            // if dept does exist, push name to dept's vec
             Entry::Occupied(mut list) => list.get_mut().push(name.to_string())
         };
     }
